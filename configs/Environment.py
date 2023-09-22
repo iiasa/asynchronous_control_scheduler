fb41f2b4f8b2c5a23a48c7331cdd1d4d15836eff
Rupesh Singh <wrufesh@gmail.com>
2023-09-22 11:37:34 +0200

    remove pydantic

--- configs/Environment.py
+++ configs/Environment.py
@@ -1,10 +1,11 @@
+import os
 from typing import Optional
 from functools import lru_cache
-from pydantic_settings import BaseSettings
 
-class AppSetting(BaseSettings):
-    CELERY_BROKER_URL:str
-    ACCELERATOR_API_URL: str
+class AppSetting():
+    def __init__(self):
+        self.CELERY_BROKER_URL:str = os.environ['CELERY_BROKER_URL']
+        self.ACCELERATOR_API_URL: str =os.environ['CELERY_BROKER_URL']
 
 @lru_cache
 def get_environment_variables():
