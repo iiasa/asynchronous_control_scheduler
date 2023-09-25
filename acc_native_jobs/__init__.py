600540167a78f32aa8d4bf2dc936d5ffda7f6e2a
Rupesh Singh <wrufesh@gmail.com>
2023-09-25 15:22:17 +0200

    add capture_log decorator

--- acc_native_jobs/__init__.py
+++ acc_native_jobs/__init__.py
@@ -1,3 +1,7 @@
+import io
+import uuid
+import traceback
+from contextlib import redirect_stdout, redirect_stderr
 from celery import Celery
 
 from accli import ACliService
@@ -11,6 +15,49 @@ env = get_environment_variables()
 app = Celery('acc_native_jobs', broker=env.CELERY_BROKER_URL)
 
 
+
+def capture_log(func):
+    """Capture stdout and stderr to accelerator data repo"""
+
+    def wrapper_func(*args, **kwargs):
+        job_token = kwargs['job_token']    
+        project_service = ACliService(
+            job_token,
+            cli_base_url=env.ACCELERATOR_API_URL
+        )
+
+        project_service.update_job_status("PROCESSING")
+
+        log_filename = f'{uuid.uuid4().hex}.log'
+        log_filepath = f'temp/{log_filename}'
+
+        with open(log_filepath, 'w') as log_stream:
+
+            with redirect_stdout(log_stream):
+                try:
+                    func()
+                except Exception as err:
+                    project_service.update_job_status("ERROR")
+                    error_message = ''.join(traceback.format_tb(err.__traceback__))
+                    log_stream.write(error_message)
+
+        with open(log_filepath, "rb") as file_stream:
+            bucket_object_id = project_service.add_filestream_as_job_output(
+                log_filename,
+                file_stream
+            )
+    
+        project_service.update_job_status("DONE")
+
+        #TODO @wrufesh delete temp file
+        
+    return wrapper_func
+
+
 @app.task
+@capture_log
 def merge_iamc(*args, **kwargs):
     iamc_verification_service = IamcVerificationService(*args, **kwargs)
+    
+
+    
