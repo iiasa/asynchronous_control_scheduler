dcd527c883ebd223caccc40f092dd3c0c33a5923
Rupesh Singh <wrufesh@gmail.com>
2024-02-15 00:52:40 +0100

    get not required env vars without raising errors if not present

--- configs/Environment.py
+++ configs/Environment.py
@@ -6,9 +6,9 @@ class AppSetting():
     def __init__(self):
         self.CELERY_BROKER_URL:str = os.environ['CELERY_BROKER_URL']
         self.ACCELERATOR_CLI_BASE_URL: str = os.environ['ACCELERATOR_CLI_BASE_URL']
-        self.IMAGE_REGISTRY_URL: str = os.environ['IMAGE_REGISTRY_URL']
-        self.IMAGE_REGISTRY_USER: str = os.environ['IMAGE_REGISTRY_USER']
-        self.IMAGE_REGISTRY_PASSWORD: str = os.environ['IMAGE_REGISTRY_PASSWORD']
+        self.IMAGE_REGISTRY_URL: str = os.environ.get('IMAGE_REGISTRY_URL')
+        self.IMAGE_REGISTRY_USER: str = os.environ.get('IMAGE_REGISTRY_USER')
+        self.IMAGE_REGISTRY_PASSWORD: str = os.environ.get('IMAGE_REGISTRY_PASSWORD')
 
 @lru_cache
 def get_environment_variables():
