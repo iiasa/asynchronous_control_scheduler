d095b65c9c4d8cfee21b8131eeea596c212fd33c
Rupesh Singh <wrufesh@gmail.com>
2024-04-02 11:24:38 +0200

    enhance celeryconfig

--- acc_native_jobs/celeryconfig.py
+++ acc_native_jobs/celeryconfig.py
@@ -4,4 +4,4 @@ env = get_environment_variables()
 
 broker_url = env.CELERY_BROKER_URL
 
-task_routes = {'acc_native_jobs.dispatch_wkube_task': {'queue': 'wkube'}}
+task_routes = {'dispatch_wkube_task': {'queue': 'wkube'}}
